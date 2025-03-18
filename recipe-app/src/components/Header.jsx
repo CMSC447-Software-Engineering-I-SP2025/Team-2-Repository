1adab9a898d644c8ae696654df80e03c86441db1
baylon03 <pascalowo027@gmail.com>
2025-03-18 17:04:24 -0400

    you can now press the logo to return to home

--- recipe-app/src/components/Header.jsx
+++ recipe-app/src/components/Header.jsx
@@ -1,6 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Header () {
     return <header>
-        <div className="logo-container"><img src="/logo-with-name.svg" /></div>
+        <div className="logo-container"> 
+        <Link to="/"> {/* Make the logo return to home */} 
+        <img src="/logo-with-name.svg" /> 
+        </Link></div>
         <h1>Digital Cookbook</h1>
         <NavBar/>
     </header>
