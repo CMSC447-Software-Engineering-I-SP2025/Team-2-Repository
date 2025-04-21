a7d316b3d47183d160a6939871e4dfece9d6077a
Nishant Singh <nsingh4@umbc.edu>
2025-04-21 17:50:56 -0400

    Reworked Entities to use Lombok
    Makes implementation easier to understand and simpler

--- src/main/java/edu/cmsc447/team2/recipe_maker/domain/entities/IngredientEntity.java
+++ src/main/java/edu/cmsc447/team2/recipe_maker/domain/entities/IngredientEntity.java
@@ -2,10 +2,18 @@ package edu.cmsc447.team2.recipe_maker.domain.entities;
 
 import jakarta.persistence.*;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "ingredients")
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class IngredientEntity {
     @Id
     @Column(name = "ingredient_id", nullable = false, updatable = false)
@@ -19,28 +27,4 @@ public class IngredientEntity {
 
     @Column(name = "image_url", length = 500)
     private String image;
-
-    // Default constructor (required by JPA)
-    public IngredientEntity() {}
-
-    // Parameterized constructor
-    public IngredientEntity(Long id, String name, String localizedName, String image) {
-        this.id = id;
-        this.name = name;
-        this.localizedName = localizedName;
-        this.image = image;
-    }
-
-    //Getters and Setters
-    public Long getId() {return id;}
-    public void setId(Long id) {this.id = id;}
-    
-    public String getName() {return name;}
-    public void setName(String name) {this.name = name;}
-    
-    public String getLocalizedName() {return localizedName;}
-    public void setLocalizedName(String localizedName) {this.localizedName = localizedName;}
-    
-    public String getImage() {return image;}
-    public void setImage(String image) {this.image = image;}
 }
--- src/main/java/edu/cmsc447/team2/recipe_maker/domain/entities/RecipeEntity.java
+++ src/main/java/edu/cmsc447/team2/recipe_maker/domain/entities/RecipeEntity.java
@@ -2,11 +2,19 @@ package edu.cmsc447.team2.recipe_maker.domain.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 // Recipe Entity for database writing
 @Entity
 @Table(name = "recipes")
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 
 // Class definition + JPA tagging.
 public class RecipeEntity {
@@ -28,36 +36,4 @@ public class RecipeEntity {
 
     @Column(name = "image_url", length = 500)
     private String image;
-
-    // Default constructor (required by JPA)
-    public RecipeEntity() {}
-
-    // Parameterized constructor
-    public RecipeEntity(Long id, String title, Integer usedIngredientCount, Integer missedIngredientCount, String instructions, String image) {
-        this.id = id;
-        this.title = title;
-        this.usedIngredientCount = usedIngredientCount;
-        this.missedIngredientCount = missedIngredientCount;
-        this.instructions = instructions;
-        this.image = image;
-    }
-
-    // Getters and setters (required for JPA)
-    public Long getId() {return id;}
-    public void setId(Long id) {this.id = id;}
-    
-    public String getTitle() {return title;}
-    public void setTitle(String title) {this.title = title;}
-    
-    public Integer getUsedIngredientCount() {return usedIngredientCount;}
-    public void setUsedIngredientCount(Integer usedIngredientCount) { this.usedIngredientCount = usedIngredientCount;}
-    
-    public Integer getMissedIngredientCount() {return missedIngredientCount;}
-    public void setMissedIngredientCount(Integer missedIngredientCount) { this.missedIngredientCount = missedIngredientCount;}
-    
-    public String getInstructions() {return instructions;}
-    public void setInstructions(String instructions) {this.instructions = instructions;}
-    
-    public String getImage() { return image; }
-    public void setImage(String image) { this.image = image; }
 }
