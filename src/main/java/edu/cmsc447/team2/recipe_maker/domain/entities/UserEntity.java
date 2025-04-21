60a73a6e6f6a4359e72f66fc1c7929593e10a50d
Nishant Singh <nsingh4@umbc.edu>
2025-04-20 22:05:35 -0400

    Add UserEntity model and UserRepository for user persistence
    Created UserEntity to represent application users
    - Has fields id (pk), username, password
    
    Created UserRepository that extends JpaRepository
    - Has standard CRUD functioanlity
    - Has a custome findByUsername method

--- src/main/java/edu/cmsc447/team2/recipe_maker/domain/entities/UserEntity.java
+++ src/main/java/edu/cmsc447/team2/recipe_maker/domain/entities/UserEntity.java
@@ -1,4 +1,24 @@
 package edu.cmsc447.team2.recipe_maker.domain.entities;
 
-public class AppUser {
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+//TODO later
+//@Table(name="Users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
 }
