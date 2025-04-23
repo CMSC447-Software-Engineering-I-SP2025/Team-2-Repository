5b7246e8b38a380a9b513b2f783d4cef16c5b07c
Nishant Singh <nsingh4@umbc.edu>
2025-04-23 19:03:13 -0400

    Modernized IngredientEntity with Lombok annotations

--- src/main/java/edu/cmsc447/team2/recipe_maker/domain/entities/IngredientEntity.java
+++ src/main/java/edu/cmsc447/team2/recipe_maker/domain/entities/IngredientEntity.java
@@ -2,9 +2,17 @@ package edu.cmsc447.team2.recipe_maker.domain.entities;
 
 import jakarta.persistence.*;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "ingredients")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class IngredientEntity {
     @Id
@@ -14,33 +22,9 @@ public class IngredientEntity {
     @Column(name = "name", length = 100, nullable = false)
     private String name;
 
-    @Column(name = "ingredient_name", length = 100)
+    @Column(name = "localized_name", length = 100)
     private String localizedName;
 
     @Column(name = "image_url", length = 500)
     private String image;
-
-    // Default constructor (required by JPA)
-    public IngredientEntity() {}
-
-    // Parameterized constructor
-    public IngredientEntity(Long id, String name, String localizedName, String image) {
-        this.id = id;
-        this.name = name;
-        this.localizedName = localizedName;
-        this.image = image;
-    }
-
-    //Getters and Setters
-    public Long getId() {return id;}
-    public void setId(Long id) {this.id = id;}
-    
-    public String getName() {return name;}
-    public void setName(String name) {this.name = name;}
-    
-    public String getLocalizedName() {return localizedName;}
-    public void setLocalizedName(String localizedName) {this.localizedName = localizedName;}
-    
-    public String getImage() {return image;}
-    public void setImage(String image) {this.image = image;}
 }
